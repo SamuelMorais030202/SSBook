4a874d2ece8513dc1d3c701b8ef726b95c0c3f93
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-10-31 17:58:23 -0300

    feat: criando header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,39 @@
+import styles from './header.module.css';
+import logo from '../../assets/logo.png';
+import lupa from '../../assets/lupa.png';
+import iconAdd from '../../assets/icon-adicionar.png';
+import iconFavorite from '../../assets/icon-coracao.png';
+import avatar from '../../assets/avatar.png';
+
 function Header() {
   return (
-    <header>
-      <h1>Header componente</h1>
+    <header className={ styles.headerContainer }>
+      <img src={ logo } alt="Logo SSBook" className={ styles.headerLogo } />
+      <label htmlFor="search-book" className={ styles.headerSearchLabel }>
+        <input
+          type="text"
+          placeholder="Busque um livro"
+          id="search-book"
+        />
+        <img src={ lupa } alt="Lupa search" />
+      </label>
+      <nav>
+        <ul className={ styles.headerNavList }>
+          <li>
+            <img src={ iconAdd } alt="Icon Add" />
+            Adicionar
+          </li>
+          <li>
+            <img src={ iconFavorite } alt="Icon Favorite" />
+            Favoritos
+          </li>
+          <hr />
+          <li>
+            <img src={ avatar } alt="Icon Avatar" />
+            Perfil
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
