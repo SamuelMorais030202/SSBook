99e0cbd8c3fec95d618b715cdb114265a4ae3dd7
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-11-01 00:45:11 -0300

    feat: criando lógica do selectTab

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,35 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import { useState } from 'react';
+import { SelectTabType } from '../../types/selectTabTypes';
+import styles from './home.module.css';
+
 function Home() {
+  const [selectTab, setSelectTab] = useState(SelectTabType.MyBooks);
+
+  const handleTabClick = (tabName : SelectTabType) => {
+    setSelectTab(tabName);
+  };
+
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className={ styles.homeContainer }>
+      <div className={ styles.homeSelectTab }>
+        <ul className={ styles.selectTab }>
+          <li
+            className={ selectTab === SelectTabType.MyBooks ? styles.active : '' }
+            onClick={ () => handleTabClick(SelectTabType.MyBooks) }
+          >
+            Meus livros
+          </li>
+          <li
+            className={ selectTab === SelectTabType.Borrowed ? styles.active : '' }
+            onClick={ () => handleTabClick(SelectTabType.Borrowed) }
+          >
+            Emprestados
+          </li>
+        </ul>
+        <hr />
+      </div>
     </div>
   );
 }
---
+++ src/types/selectTabTypes.ts
@@ -0,0 +1,4 @@
+export enum SelectTabType {
+  'MyBooks',
+  'Borrowed',
+}
