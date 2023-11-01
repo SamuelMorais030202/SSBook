548fd865af71b1fcef980f7a2ea4167ab6a3e28e
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-11-01 01:40:36 -0300

    feat: correção de layout mobile e iniciando a criação do card de livros favoritos

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -30,6 +30,13 @@ function Home() {
         </ul>
         <hr className={ styles.tabSeparator } />
       </div>
+
+      <div className={ styles.favoritesBooksContainer }>
+        <div className={ styles.favoritesBooksHeader }>
+          <h2>Livros favoritos</h2>
+          <span>ver todos</span>
+        </div>
+      </div>
     </div>
   );
 }
